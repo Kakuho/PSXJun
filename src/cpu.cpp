29b3e2442fbf3b7d99a5290551549161fb50e2ef
ka <kaku.h9@gmail.com>
2023-10-08 18:31:40 +0100

    Implemented a switch table for CPU decoding.

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,6 +1,139 @@
-std::
 
-// The MIPS ISA contains fixed size 32-bit instructions.
-std::uint32_t CPU::GetIntruction() const{
-  std::uint32_t buffer = GetWord(ram[pc[0]], ram[pc[1]], ram[pc[2]], ram[pc[3]]);
+// Registers
+
+std::uint32_t CPU::GetRegisterValue(std::uint8_t index) const{
+  if(index == 0){
+    // zeroth register is hardwired to 0
+    return 0;
+  }
+  else if (index <= 32){
+    return m_registers[index];
+  }
+  else{
+    /* TODO: should throw an out of bounds exception */
+    std::cerr << "ERROR :: INDEX IS OUT OF BOUNDS" << '\n';
+  }
 }
+
+// Memory getters
+
+std::uint8_t CPU::ReadByte(std::index) const{
+  /* TODO: wrapping around the address space*/
+  return m_ram[index];
+}
+
+std::uint16_t CPU::GetHalfWord(std::size_t index1, std::size_t index2, 
+                               std::size_t index3, std::size_t index4) const{
+  /* TODO : wrapping around the address space */
+  // Assumes: fetching bytes in the order M[index1], M[index2], M[index3], M[index4]
+  std::uint8_t byte1 = ReadByte(index1);
+  // R3000A is little endian, so the half word would be M[index4], M[index3], M[index2], M[index1].
+}
+
+std::uint32_t CPU::GetWord(std::size_t index) const{
+
+}
+
+
+std::uint32_t CPU::GetIntruction() const{
+  // The MIPS ISA contains fixed size 32-bit instructions.
+  std::uint32_t buffer = GetWord(ram[pc]);
+}
+
+// CPU operational functions
+//---------------------------------------------------------------//
+// Classic Fetch Decode Cycle:
+//
+// The CPU contains a buffer of ticks (m_ticks) which contains the amount of ticks
+// needed / stored for each executed instruction. Tick() checks to see if there are any 
+// pending ticks and proceeds only if there are no more ticks left in the buffer.
+//
+// Perhaps not the most cycle accurate, however later on I will do a survery to see how other 
+// emulators handle cycle accuracy
+//
+//  * Fetch(): fetches the instruction from memory.
+//
+//    ^
+//    |
+//
+//  * Tick(): checks to see if there is any buffered ticks. 
+//            If not then proceeds to Fetch(), decode and executes the instruction.
+//            Otherwise a call decrements the buffered ticks.
+//
+// 
+//---------------------------------------------------------------//
+//
+// However the R3000A is a mips-1 processor and thus implements a 5 stage pipeline.
+// Therefore more research is needed to understand how to create an emulator for a 
+// 5-stage pipeline, as I expect timing considerations will be different, especially in regards
+// to data hazards.
+//
+//---------------------------------------------------------------//
+
+void CPU::Tick(){
+  // first check if there are any buffered ticks
+  if( GetTicks() ){
+    return;
+  }
+  else{
+    Fetch();
+    std::uint8_t opcode = Instruction::GetOpcode(GetInstruction());  // decode step
+    switch(opcode){
+      case 0: // R-type instruction
+        std::uint8_t funct = instruction::GetFunct(GetInstruction());
+        switch(funct){
+          case OP_ADD:    UnimplementedOp(); break;
+          case OP_ADDU:   UnimplementedOp(); break;
+          case OP_SUB:    UnimplementedOp(); break;
+          case OP_SUBU:   UnimplementedOp(); break;
+          case OP_MULT:   UnimplementedOp(); break;
+          case OP_MULTU:  UnimplementedOp(); break;
+          case OP_DIV:    UnimplementedOp(); break;
+          case OP_DIVU:   UnimplementedOp(); break;
+          case OP_MFHI:   UnimplementedOp(); break;
+          case OP_MTHI:   UnimplementedOp(); break;
+          case OP_MFLO:   UnimplementedOp(); break;
+          case OP_MTLO:   UnimplementedOp(); break;
+          case OP_AND:    UnimplementedOp(); break;
+          case OP_NOR:    UnimplementedOp(); break;
+          case OP_XOR:    UnimplementedOp(); break;
+          case OP_OR:     UnimplementedOp(); break;
+          case OP_JALR:   UnimplementedOp(); break;
+          case OP_JR:     UnimplementedOp(); break;
+          case OP_SLT:    UnimplementedOp(); break;
+          case OP_SLTU:   UnimplementedOp(); break;
+          case OP_SLL:    UnimplementedOp(); break;
+          case OP_SRL:    UnimplementedOp(); break;
+          case OP_SRA:    UnimplementedOp(); break;
+          case OP_MFC0:   UnimplementedOp(); break;
+        }
+      // all other functions
+      case OP_JP:     UnimplementedOp(); break;
+      case OP_JAL:    UnimplementedOp(); break;
+      case SP_SLTI:   UnimplementedOp(); break;
+      case SP_SLTIU:  UnimplementedOp(); break;
+      case SP_ANDI:   UnimplementedOp(); break;
+      case SP_ORI:    UnimplementedOp(); break;
+      case SP_BEQ:    UnimplementedOp(); break;
+      case SP_BNE:    UnimplementedOp(); break;
+      case SP_BLEZ:   UnimplementedOp(); break;
+      case SP_BGTZL   UnimplementedOp(); break;
+      case SP_ADDI:   UnimplementedOp(); break;
+      case SP_ADDIU:  UnimplementedOp(); break;
+      case SP_LUI:    UnimplementedOp(); break;
+      case SP_SW:     UnimplementedOp(); break;
+      case SP_LB:     UnimplementedOp(); break;
+      case SP_LW:     UnimplementedOp(); break;
+      case SP_LBU:    UnimplementedOp(); break;
+      case SP_LHU:    UnimplementedOp(); break;
+      case SP_SB:     UnimplementedOp(); break;
+      case SP_SH:     UnimplementedOp(); break;
+    }
+  }
+}
+
+void CPU::Fetch(){
+  SetInstruction( ReadWord( GetPC() ) );
+}
+
+// CPU opcodes functions
--- src/cpu.hpp
+++ src/cpu.hpp
@@ -1,32 +1,65 @@
 // Definition of the r3000a processor.
 #include <array>
+#include <iostream>
 
-static const long CLOCKSPEED = 1;
+#include "instruction.hpp"
+
+static const long CLOCKSPEED = -1;
 
 // The PSX CPU is a MIPS R3000A cpu.
 class CPU{
-  // private vars
   std::array<std::uint8_t, 4000> m_instrucion_cache;
   std::array<std::uint8_t, 1000> m_data_cache;
   std::array<std::uint32_t 32>   m_registers;
+  /* TODO: memory should be moved into its own class */
+  std::array<std::uint8_t 100000> m_ram;  
   std::uint32_t m_hi = 0;     // upper half of 64 bit multiplication
   std::uint32_t m_low = 0;    // lower half of 64 bit multiplication
   std::uint32_t m_pc;
+  std::size_t   m_ticks;
   std::uint32_t m_ibuffer     // buffer for the current instruction
 
   public:
-  // Avoiding initialisation for now :/
-  std::array<std::uint8_t 100000> ram; // for now, I keep ram in here however it will move later on
-  std::uint32_t GetRegister(std::size_t index) const{ return m_registers[index]; }
-  void Tick();
-  // N.B all memory input / output instructions are written on the Data Cache
-  // Data access - Note Little Endian
+  /* TODO: create initialisation functions */
+
+    // register related
+  
+  std::uint32_t Register(std::uint8_t index) const { return m_registers[index]; }
+  std::uint32_t& Register(std::uint8_t index){ return m_registers[index]; }
+  std::uint32_t GetRegisterValue(std::uint8_t index) const;
+
+  std::uint32_t GetPC() const { return m_pc;}
+  void SetPC(std::uint32_t addr){ m_pc = addr;}
+
+  std::uint32_t GetHi() const { return m_hi;}
+  std::uint32_t GetLo() const { return m_lo;}
+
+    // instruction buffer functions
+
+  void SetInstruction(std::uint32_t value){ m_ibuffer = value;}
+  std::uint32_t GetInstruction() const{ return m_ibuffer;}
+
+    // N.B  : memory input / output instructions are written to the Data Cache
+    //      : r3000a is a little endian processor 
+    //      : r3000a defines a byte as 8 bit, halfword as 16 bits and a word as 32 bits
+    
   std::uint8_t ReadByte(std::size_t index) const;
-  std::uint16_t GetHalfWord(std::size_t index1, std::size_t index2, std::size_t index3, std::size_t index3) const;
+  std::uint16_t GetHalfWord(std::size_t index1, std::size_t index2, 
+                            std::size_t index3, std::size_t index3) const;
   std::uint32_t GetWord(std::size_t index) const;
   std::uint32_t GetInstruction() const;
-  // Data Read
-  void WriteData();
-  // D_Cache
-  void ReadIntoDCache;
+
+    // CPU operational functions
+
+  void Tick();
+  std::size_t GetTicks() const{ return m_ticks; }
+  void Fetch();
+  void Decode();
+  void Execute();
+
+    // CPU opcodes
+
+  void UnimplementedOp() const{ std::cout << "OPERATION NOT IMPLEMENTED!" << '\n';} // helper
+  void InvalidOp() const{ std::cout << "INSTRUCTION IS INVALID" << '\n'; }
+
 }
